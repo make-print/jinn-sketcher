6fc49a086930a4cf7f6e010b62fe47bbdba5fe89
Technocracy <erud1t3.devs@gmail.com>
2024-05-27 11:45:14 -0400

    updates to skunkworks

---
+++ skunkworks/sources/jinnInference/hf_test_inference.py
@@ -0,0 +1,19 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
+from huggingface_hub import login
+
+# Login to Hugging Face Hub
+login(token="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")
+
+model_id = "google/paligemma-3b-mix-224"
+model = PaliGemmaForConditionalGeneration.from_pretrained(model_id)
+processor = AutoProcessor.from_pretrained(model_id)
+
+prompt = "What is on the flower?"
+image_file = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg?download=true"
+raw_image = Image.open(requests.get(image_file, stream=True).raw)
+inputs = processor(text=prompt, images=raw_image, return_tensors="pt")
+output = model.generate(**inputs, max_new_tokens=20)
+
+print(processor.decode(output[0], skip_special_tokens=True)[len(prompt):])
